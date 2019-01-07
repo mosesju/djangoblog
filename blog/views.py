8a057a2a3c5f8f75b1b3bf01692ad6202a67bfc3
mosesju <edwinjmoses@lewisu.edu>
2019-01-06 19:41:23 -0600

    This version queries the database and puts values into the template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.utils import timezone
 # Create your views here.
+def post_list(request):
+	post = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	#The line above puts the query into the variable into post
+	#The line below turns the variable post into post in the html template
+	return render(request, 'blog/post_list.html',{'posts':post})
